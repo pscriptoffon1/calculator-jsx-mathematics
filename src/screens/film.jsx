33ba717a0f30fd708d5b045f88ce33c4fe43d09a
pscriptoffon0 <pscriptoffon1@users.noreply.github.com>
2026-07-06 19:43:19 +0600

    update

--- src/screens/film.jsx
+++ src/screens/film.jsx
@@ -1,192 +1,102 @@
 import { useState } from "react";
 
-
-
-
 function Film() {
 
     let [result, setResult] = useState(0);
+    let [result2, setResult2] = useState("");
+    let [result3, setResult3] = useState("");
     let [sel, setSel] = useState("1");
 
+    function calcPrice(e, seat) {
 
-
-    function calcPrice(e) {
         let price = 0;
+        let time = "";
 
         if (sel === "1") {
             price = 300;
+            time = "12:00";
         }
 
         if (sel === "2") {
             price = 350;
+            time = "17:00";
         }
 
         if (sel === "3") {
             price = 400;
+            time = "20:00";
         }
+
+        setResult3(time);
 
         if (e.target.checked) {
             setResult(result + price);
-        } else {
+            setResult2(seat);
+        }
+        else {
             setResult(result - price);
+            setResult2("");
         }
     }
 
     return (
         <div className="all">
+
             <div className="page1">
 
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-            </div>
-            <div className="page2">
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,1)}
+                />
 
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" type="checkbox" onChange={calcPrice} name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" onChange={calcPrice} id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,2)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,3)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,4)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,5)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,6)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,7)}
+                />
+
+                <input className="ch1" type="checkbox"
+                    onChange={(e)=>calcPrice(e,8)}
+                />
 
             </div>
-            <div className="page1">
 
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-            </div>
-            <div className="page1">
-
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-            </div>
-
-            <div className="page2">
-
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" type="checkbox" onChange={calcPrice} name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" onChange={calcPrice} id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-                <input className="ch1" onChange={calcPrice} type="checkbox" name="" id="" />
-
-            </div>
             <div>
+
                 <select
                     value={sel}
-                    onChange={(e) => {
-                        setSel(e.target.value);
-                        setResult(0); // сд цену 0
-                    }}
+                    onChange={(e)=>setSel(e.target.value)}
                 >
                     <option value="1">12:00</option>
                     <option value="2">17:00</option>
                     <option value="3">20:00</option>
                 </select>
 
-                <h1>{result} ₽</h1>
+                <h1>Цена: {result} ₽</h1>
+                <h1>Место: {result2}</h1>
+                <h1>Время: {result3}</h1>
+
             </div>
+
         </div>
     );
 }
 
 export default Film;
-
-
-
